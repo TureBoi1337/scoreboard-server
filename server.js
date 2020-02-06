f21569d3fa1786e6efe143d4930110dcb3d50166
Ture <ture.danielsson@gmail.com>
2020-02-06 13:34:12 +0100

    Adde libs, set up server.

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 
 
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = (process.env.PORT || 5000);
+app.use(helmet());
+app.use(bodyParser.json({type: 'application/*+json'}));
 
+app.use(morgan("dev"));
+
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin" ,"*");
+    res.header("Acces.Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept" );
+    next();
+});
+
+
+
+const  homeRoute = require ("./routes/home");
+app.use(homeRoute);
+
+const  userRoute = require ("./routes/user");
+app.use(userRoute);
 
 
 app.listen(PORT, () =>{
