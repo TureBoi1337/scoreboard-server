ce653ce6d7a5bae157d7129b91121b573a017bcc
Ture <ture.danielsson@gmail.com>
2020-02-06 13:34:51 +0100

    Created home route.

---
+++ routes/home.js
@@ -0,0 +1,16 @@
+
+
+
+const express = require ("express");
+const router = express.Router();
+
+
+router.get("/",(req, res)=>{
+    res.send("<h1> Hello World</h");
+});
+
+
+
+
+
+module.exports = router;
